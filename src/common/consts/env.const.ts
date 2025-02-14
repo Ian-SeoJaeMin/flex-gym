370f8ab25c566bafcc4f3a5d753697418c6dcacb
Ian-SeoJaeMin <animomin@daum.net>
2025-02-14 19:39:38 +0900

    NestJS와 TypeORM 설정으로 초기 프로젝트 구성
    - 프로젝트 설정 파일 수정(package.json, tsconfig, nest-cli.json)
    - ESLint와 Prettier 수정
    - Docker Compose 설정
    - TypeORM과 ConfigModule 설정 및 DB 연동 테스트

---
+++ src/common/consts/env.const.ts
@@ -0,0 +1,10 @@
+export const envVariableKeys = {
+    port: 'PORT',
+    dbHost: 'DB_HOST',
+    dbPort: 'DB_PORT',
+    dbUsername: 'DB_USERNAME',
+    dbPassword: 'DB_PASSWORD',
+    dbDatabase: 'DB_DATABASE',
+    jwtSecret: 'JWT_SECRET',
+    jwtExpiresIn: 'JWT_EXPIRES_IN'
+};
---
+++ src/common/provider/database.provider.ts
@@ -0,0 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { envVariableKeys } from '../consts/env.const';
+
+export const databaseProvider = [
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+            const isDevelopment = configService.get<string>('NODE_ENV') === 'development';
+            return {
+                type: 'postgres',
+                host: configService.get<string>(envVariableKeys.dbHost),
+                port: configService.get<number>(envVariableKeys.dbPort),
+                username: configService.get<string>(envVariableKeys.dbUsername),
+                password: configService.get<string>(envVariableKeys.dbPassword),
+                database: configService.get<string>(envVariableKeys.dbDatabase),
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                synchronize: isDevelopment,
+                logging: true
+            } as TypeOrmModuleOptions;
+        }
+    })
+];
