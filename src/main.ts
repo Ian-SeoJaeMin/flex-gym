cccefd43fdf0fa1bb9792ed7dc9ba944cfeb36e7
Ian-SeoJaeMin <animomin@daum.net>
2025-02-16 22:51:30 +0900

    feat: Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     const configService = app.get(ConfigService);
     const port = configService.get<number>('PORT');
+
+    swaggerConfig(app);
 
     console.log(`Server is running on port ${port}`);
     await app.listen(port);
